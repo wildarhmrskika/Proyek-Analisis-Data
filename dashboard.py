cae3818a619b174490d681aaeb65ea47ecde572e
Wilda Rahma Riskika <150354847+wildarhmrskika@users.noreply.github.com>
2024-10-03 10:27:26 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -8,7 +8,7 @@ from sklearn.ensemble import IsolationForest
 
 # Fungsi untuk memuat data day.csv
 def load_day_data():
-    file_path_day = r'data/day.csv'
+    file_path_day = (r'data/day.csv')
     if os.path.exists(file_path_day):
         df_day = pd.read_csv(file_path_day)
         return df_day
@@ -18,7 +18,7 @@ def load_day_data():
 
 # Fungsi untuk memuat data hour.csv
 def load_hour_data():
-    file_path_hour = r'data/hour.csv
+    file_path_hour = (r'data/hour.csv)
     if os.path.exists(file_path_hour):
         df_hour = pd.read_csv(file_path_hour)
         return df_hour
